090b21d40fd332b1bb4d3aad8c04928901137a57
Leah Restad (they/them) <leahrestad@microsoft.com>
2026-04-28 14:40:42 -0700

    debug

--- nodejs/src/URIValidator.ts
+++ nodejs/src/URIValidator.ts
@@ -1,3 +1,6 @@
+// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT License.
+
 import { domainToASCII } from "url";
 
 import { _azureKeyVaultDomains, _azureStorageDomains } from "./Helpers/Domains";
